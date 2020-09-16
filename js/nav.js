bd57945f2389143425116a021b453c9b8f9dd2bd
Ollie-hub <Oliv585p@elev.techcollege.dk>
2020-09-16 15:07:56 +0200

    .

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
-const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".navbar")
+const menuIcon = document.querySelector(".hamburger-menu"); // laver variabelen hamburger-menu og linker den op mod klassen "hamburger-menu"
+const navbar = document.querySelector(".navbar") // laver variablen navbar og linker den op mod klassen "navbar"
 
-menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("change");
+menuIcon.addEventListener("click", () => { // Ligger en eventlistener "click" på variablen menuIcon
+    navbar.classList.toggle("change"); // ligger en toggle på navbar klassen som skal ligge og fjerne "change" klassen
 });

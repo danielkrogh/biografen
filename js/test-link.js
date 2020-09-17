fde7891464dfdbdda18ceb4d72ac03fff7b99325
Daniel Krogh Andersen <daniel@kroghkommunikation.dk>
2020-09-17 11:03:46 +0200

    .

--- js/test-link.js
+++ js/test-link.js
@@ -2,14 +2,14 @@
     Knap der tilføjer data til local storage udfra html attribut data-title
 */
 let buyBtn = document.querySelectorAll('.buy');
-let occupiedDruk = [20, 21, 39, 40]; // Optagede pladser til filmen 'Druk'
-let occupiedAntebellum = [16, 17, 18, 19, 42, 43, 44, 45]; // Optagede pladser til filmen 'Antebellum'
+let occupiedTrash = [20, 21, 39, 40]; // Optagede pladser til filmen 'Trash'
+let occupiedRiot = [16, 17, 18, 19, 42, 43, 44, 45]; // Optagede pladser til filmen 'Riot'
 
 buyBtn.forEach(elm => elm.addEventListener('click', () => {
     localStorage.setItem('movieTitle', event.target.dataset.title); // Filmes titel føjes til local storage
-    if (event.target.dataset.title == 'druk') { // Hvis filems titel er druk tilføjes optagede pladser til filmen 'Druk' til local storage
-        localStorage.setItem('occupiedSeats', JSON.stringify(occupiedDruk));
-    } else if (event.target.dataset.title == 'antebellum') { // Hvis filems titel er antebellum tilføjes optagede pladser til filmen 'Antebellum' til local storage
-        localStorage.setItem('occupiedSeats', JSON.stringify(occupiedAntebellum));
+    if (event.target.dataset.title == 'trash') { // Hvis filems titel er trash tilføjes optagede pladser til filmen 'Trash' til local storage
+        localStorage.setItem('occupiedSeats', JSON.stringify(occupiedTrash));
+    } else if (event.target.dataset.title == 'riot') { // Hvis filems titel er riot tilføjes optagede pladser til filmen 'Riot' til local storage
+        localStorage.setItem('occupiedSeats', JSON.stringify(occupiedRiot));
     }
 }))

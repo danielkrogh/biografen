8ed466a44c5fad4d0429664902304a1d651e1dea
Markus  backhausen <markusbackhausen@outlook.com>
2020-09-24 06:55:58 +0200

    mb

--- js/movieArr.js
+++ js/movieArr.js
@@ -8,7 +8,7 @@ let arrAction = [
         bio: '1',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     {   
         title: 'citizenfour',
@@ -19,7 +19,7 @@ let arrAction = [
         bio: '2',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Dokumentarfilmen 'Citizenfour' handler om whistlebloweren Edward Snowden. Dokumentaristen Laura Poitras, var med helt fra starten og portrætterer Snowden gennem adskillige møder medFilmen tager sin begyndelse da Poitras flyver til Hong Kong, for at møde manden bag en række anonyme mails hun har modtaget. Det eneste de var underskrevet med var: Citizen Four. Han var klar til at afsløre hvordan NSA m.fl. benyttede sig af skjult overvågning. Produktet er en thriller, der udfolder sig foran tilskueren." 
     },
     {   
         title: 'trash',
@@ -30,7 +30,7 @@ let arrAction = [
         bio: '3',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     {   
         title: 'trash',
@@ -41,7 +41,7 @@ let arrAction = [
         bio: '1',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     
 ]
@@ -50,34 +50,34 @@ let arrDrama = [
         title: 'trash',
         image: 'Poster-Trash',
         trailer: 'Citizenfour.mp4',
-        id: '3',
+        id: '0',
         time: '116',
         bio: '2',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     {   
         title: 'trash',
         image: 'Poster-Trash',
         trailer: 'Citizenfour.mp4',
-        id: '4',
+        id: '1',
         time: '116',
         bio: '1',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     {   
         title: 'trash',
         image: 'Poster-Trash',
         trailer: 'Citizenfour.mp4',
-        id: '5',
+        id: '2',
         time: '116',
         bio: '3',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     }  
 ]
 let arrBørn = [
@@ -85,44 +85,44 @@ let arrBørn = [
         title: 'trash',
         image: 'Poster-Trash',
         trailer: 'Citizenfour.mp4',
-        id: '6',
+        id: '0',
         time: '116',
         bio: '1',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     {   
         title: 'trash',
         image: 'Poster-Trash',
         trailer: 'Citizenfour.mp4',
-        id: '7',
+        id: '1',
         time: '116',
         bio: '2',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     {   
         title: 'trash',
         image: 'Poster-Trash',
         trailer: 'Citizenfour.mp4',
-        id: '8',
+        id: '2',
         time: '116',
         bio: '1',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     },
     {   
         title: 'trash',
         image: 'Poster-Trash',
         trailer: 'Citizenfour.mp4',
-        id: '9',
+        id: '3',
         time: '116',
         bio: '3',
         cate: 'børn',
         actor:'John doe',
-        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, voluptatem ut? Officia et quidem alias.' 
+        desc: "Det polske drama 'Ida' handler om en 17-årig pige, der i 1960ernes Polen er på vej til at blive nonne. Ida er mere eller mindre vokset op på klostret og har ingen kontakt til sin familie. Alligevel føler den unge novice et stærkt behov for at opsøge sin eneste slægtning, morens søster Wanda, før hun tager sit klosterløfte. Mosteren besidder en magtfuld stilling som højesteretsdommer, men lever derudover et lettere alkoholiseret og løssluppent liv langt fra Idas puritanske hverdag. Hun har gang på gang afvist sin nieces ønske om at komme på besøg, og da Ida pludseligt står uden for døren med et kors om halsen, og en beskeden bagage under armen, lukker Wanda hende modvilligt ind. Snart opdager Ida sandheden om sin familie, der forsvandt under besættelsestiden, og i fællesskab med sin moster rejser den unge pige nu tilbage til sin fødeby for at afdække deres tragiske familiehistorie og muligvis lære sig selv bedre at kende. ’Ida’ er filmet i smukke sort/hvide billeder og kombinerer på én gang de barske realiteter fra jødeforfølgelsen under Anden Verdenskrig med fortællingen om en ung piges (dragende spillet af Agata Trzebuchowska) religiøse identitet samt spirende seksualitet. Filmen, der er årets Oscar-kandidat fra Polen, indeholder desuden genregreb fra den klassiske road movie og er instrueret af Pawel Pawlikowski, der tidligere har stået bag ’My Summer of Love’ og ’The Woman in the Fifth’." 
     }
 ];
--- js/valgtFilm.js
+++ js/valgtFilm.js
@@ -1,5 +1,6 @@
 const poster = document.querySelector('.poster');
 const desc = document.querySelector('.post-desc');
+const trailerTitle = document.querySelector('.trailer-title');
 const video = document.querySelector('.video-source');
 const postTitle = document.querySelector('.post-title');
 newArray = JSON.parse(sessionStorage.getItem('newArray'));
@@ -9,9 +10,24 @@ function movieDesc(arr) {
         // ALT TAG
         //Description
         desc.innerHTML = arr.desc;
-        postTitle.innerHTML = arr.image;
+        postTitle.innerHTML = arr.image.slice(7);
         // Trailer
         video.src = `mov/${arr.trailer}`;
+        console.log(arr.image)
+        if(arr.image.slice(7) == "Trash") {
+                postTitle.style.color = "yellow"
+        }
+        function myFunction(x) {
+                if (x.matches) { // If media query matches
+                document.querySelector('.overlay').style.backgroundImage = `url("img/${arr.image}.jpg")`;
+                } else {
+                        document.querySelector('.overlay').style.backgroundImage = `url("")`;
+                }
+              }
+              
+              var x = window.matchMedia("(max-width: 900px)")
+              myFunction(x) // Call listener function at run time
+              x.addListener(myFunction)
 }
 
 movieDesc(newArray);
